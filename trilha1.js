c791ee72f91cbd98519d405d05fd637fdf56d715
desbika <enzogamenezes@gmail.com>
2025-11-17 20:45:44 -0300

    first commit

---
+++ trilha1.js
@@ -0,0 +1,77 @@
+class ContaBancaria {
+    constructor(titular, saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    depositar(valor) {
+        this.saldo += valor;
+        console.log(`Depósito de R$${valor} realizado. Novo saldo: R$${this.saldo}`);
+    }
+
+    sacar(valor) {
+        if (valor > this.saldo) {
+            console.log("Saldo insuficiente!");
+            return;
+        }
+        this.saldo -= valor;
+        console.log(`Saque de R$${valor} realizado. Saldo restante: R$${this.saldo}`);
+    }
+}
+
+class Livro {
+    constructor(titulo, autor, paginas, lido = false) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+        this.lido = lido;
+    }
+
+    marcarComoLido() {
+        this.lido = true;
+        console.log(`Você marcou o livro "${this.titulo}" como lido.`);
+    }
+}
+
+class Produto {
+    constructor(nome, preco, quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    valorTotal() {
+        return this.preco * this.quantidade;
+    }
+}
+
+class Temperatura {
+    constructor(valor) {
+        this.valor = valor; // valor em Celsius
+    }
+
+    paraFahrenheit() {
+        return (this.valor * 9/5) + 32;
+    }
+
+    paraKelvin() {
+        return this.valor + 273.15;
+    }
+}
+
+class Agenda {
+    constructor() {
+        this.compromissos = [];
+    }
+
+    adicionar(compromisso) {
+        this.compromissos.push(compromisso);
+    }
+
+    listar() {
+        console.log("📅 Compromissos:");
+        this.compromissos.forEach((c, i) => {
+            console.log(`${i + 1}. ${c}`);
+        });
+    }
+}
